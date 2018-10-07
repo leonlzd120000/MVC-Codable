2826bb97620c2bcebc29948f2cf182a4dd16ad0b
leonlzd120000 <leonlzd120000@gamil.com>
2018-10-07 12:33:18 +0800

    first commit

---
+++ MVCModel/Item.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  MVCModel
+//
+//  Created by leon on 2018/10/6.
+//  Copyright © 2018年 leonlee. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    
+    var done  : Bool = false
+    
+}
